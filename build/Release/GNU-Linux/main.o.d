e1c17a0a415f90e0c9aef7938f25c8ab8d77ec05
abulayek <abulayek@yahoo.com>
2017-01-15 23:08:11 +0900

    All Files

---
+++ build/Release/GNU-Linux/main.o.d
@@ -0,0 +1,135 @@
+build/Release/GNU-Linux/main.o: main.cpp \
+ /home/layek/ffmpeg_build/include/libavutil/opt.h \
+ /home/layek/ffmpeg_build/include/libavutil/rational.h \
+ /home/layek/ffmpeg_build/include/libavutil/attributes.h \
+ /home/layek/ffmpeg_build/include/libavutil/avutil.h \
+ /home/layek/ffmpeg_build/include/libavutil/common.h \
+ /home/layek/ffmpeg_build/include/libavutil/macros.h \
+ /home/layek/ffmpeg_build/include/libavutil/version.h \
+ /home/layek/ffmpeg_build/include/libavutil/avconfig.h \
+ /home/layek/ffmpeg_build/include/libavutil/mem.h \
+ /home/layek/ffmpeg_build/include/libavutil/error.h \
+ /home/layek/ffmpeg_build/include/libavutil/mathematics.h \
+ /home/layek/ffmpeg_build/include/libavutil/intfloat.h \
+ /home/layek/ffmpeg_build/include/libavutil/log.h \
+ /home/layek/ffmpeg_build/include/libavutil/pixfmt.h \
+ /home/layek/ffmpeg_build/include/libavutil/dict.h \
+ /home/layek/ffmpeg_build/include/libavutil/samplefmt.h \
+ /home/layek/ffmpeg_build/include/libavcodec/avcodec.h \
+ /home/layek/ffmpeg_build/include/libavutil/samplefmt.h \
+ /home/layek/ffmpeg_build/include/libavutil/attributes.h \
+ /home/layek/ffmpeg_build/include/libavutil/avutil.h \
+ /home/layek/ffmpeg_build/include/libavutil/buffer.h \
+ /home/layek/ffmpeg_build/include/libavutil/cpu.h \
+ /home/layek/ffmpeg_build/include/libavutil/channel_layout.h \
+ /home/layek/ffmpeg_build/include/libavutil/dict.h \
+ /home/layek/ffmpeg_build/include/libavutil/frame.h \
+ /home/layek/ffmpeg_build/include/libavutil/buffer.h \
+ /home/layek/ffmpeg_build/include/libavutil/log.h \
+ /home/layek/ffmpeg_build/include/libavutil/pixfmt.h \
+ /home/layek/ffmpeg_build/include/libavutil/rational.h \
+ /home/layek/ffmpeg_build/include/libavcodec/version.h \
+ /home/layek/ffmpeg_build/include/libavutil/version.h \
+ /home/layek/ffmpeg_build/include/libavutil/common.h \
+ /home/layek/ffmpeg_build/include/libavutil/imgutils.h \
+ /home/layek/ffmpeg_build/include/libavutil/pixdesc.h \
+ /home/layek/ffmpeg_build/include/libavutil/mathematics.h \
+ /home/layek/ffmpeg_build/include/libavformat/avformat.h \
+ /home/layek/ffmpeg_build/include/libavformat/avio.h \
+ /home/layek/ffmpeg_build/include/libavformat/version.h encode_video.h \
+ /home/layek/ffmpeg_build/include/libavutil/avassert.h \
+ /home/layek/ffmpeg_build/include/libavutil/timestamp.h \
+ /home/layek/ffmpeg_build/include/libswscale/swscale.h \
+ /home/layek/ffmpeg_build/include/libswscale/version.h \
+ /home/layek/ffmpeg_build/include/libswresample/swresample.h \
+ /home/layek/ffmpeg_build/include/libswresample/version.h
+
+/home/layek/ffmpeg_build/include/libavutil/opt.h:
+
+/home/layek/ffmpeg_build/include/libavutil/rational.h:
+
+/home/layek/ffmpeg_build/include/libavutil/attributes.h:
+
+/home/layek/ffmpeg_build/include/libavutil/avutil.h:
+
+/home/layek/ffmpeg_build/include/libavutil/common.h:
+
+/home/layek/ffmpeg_build/include/libavutil/macros.h:
+
+/home/layek/ffmpeg_build/include/libavutil/version.h:
+
+/home/layek/ffmpeg_build/include/libavutil/avconfig.h:
+
+/home/layek/ffmpeg_build/include/libavutil/mem.h:
+
+/home/layek/ffmpeg_build/include/libavutil/error.h:
+
+/home/layek/ffmpeg_build/include/libavutil/mathematics.h:
+
+/home/layek/ffmpeg_build/include/libavutil/intfloat.h:
+
+/home/layek/ffmpeg_build/include/libavutil/log.h:
+
+/home/layek/ffmpeg_build/include/libavutil/pixfmt.h:
+
+/home/layek/ffmpeg_build/include/libavutil/dict.h:
+
+/home/layek/ffmpeg_build/include/libavutil/samplefmt.h:
+
+/home/layek/ffmpeg_build/include/libavcodec/avcodec.h:
+
+/home/layek/ffmpeg_build/include/libavutil/samplefmt.h:
+
+/home/layek/ffmpeg_build/include/libavutil/attributes.h:
+
+/home/layek/ffmpeg_build/include/libavutil/avutil.h:
+
+/home/layek/ffmpeg_build/include/libavutil/buffer.h:
+
+/home/layek/ffmpeg_build/include/libavutil/cpu.h:
+
+/home/layek/ffmpeg_build/include/libavutil/channel_layout.h:
+
+/home/layek/ffmpeg_build/include/libavutil/dict.h:
+
+/home/layek/ffmpeg_build/include/libavutil/frame.h:
+
+/home/layek/ffmpeg_build/include/libavutil/buffer.h:
+
+/home/layek/ffmpeg_build/include/libavutil/log.h:
+
+/home/layek/ffmpeg_build/include/libavutil/pixfmt.h:
+
+/home/layek/ffmpeg_build/include/libavutil/rational.h:
+
+/home/layek/ffmpeg_build/include/libavcodec/version.h:
+
+/home/layek/ffmpeg_build/include/libavutil/version.h:
+
+/home/layek/ffmpeg_build/include/libavutil/common.h:
+
+/home/layek/ffmpeg_build/include/libavutil/imgutils.h:
+
+/home/layek/ffmpeg_build/include/libavutil/pixdesc.h:
+
+/home/layek/ffmpeg_build/include/libavutil/mathematics.h:
+
+/home/layek/ffmpeg_build/include/libavformat/avformat.h:
+
+/home/layek/ffmpeg_build/include/libavformat/avio.h:
+
+/home/layek/ffmpeg_build/include/libavformat/version.h:
+
+encode_video.h:
+
+/home/layek/ffmpeg_build/include/libavutil/avassert.h:
+
+/home/layek/ffmpeg_build/include/libavutil/timestamp.h:
+
+/home/layek/ffmpeg_build/include/libswscale/swscale.h:
+
+/home/layek/ffmpeg_build/include/libswscale/version.h:
+
+/home/layek/ffmpeg_build/include/libswresample/swresample.h:
+
+/home/layek/ffmpeg_build/include/libswresample/version.h:
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 /* 
  * File:   main.cpp
+ * Author: quangthai
  *
  * Created on August 3, 2016, 5:16 PM
  */
